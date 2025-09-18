d0e63da8061c0758d07f73dbf09aa4480c2c82bb
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-09-18 13:19:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+function generateLink() {
+	let name = document.getElementById("name").value;
+	let year = document.getElementById("year").value;
+	let url = document.getElementById("url");
+	let baseUrl = "https://localhost:8080/";
+
+	let finalUrl = baseUrl + `?name=${name}&year=${year}`;
+	url.innerText = finalUrl;
+	
+}
